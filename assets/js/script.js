2f2ad594acf7f223e3762f39103e1a2fb0774105
ktalimaa <kristeltalimaa@gmail.com>
2023-02-02 19:27:21 +0200

    JS added

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// alert("Hello, SDA!");       /* function box. no so much used nowadays. */
+
+console.log("I may be a Front-end Developer!")  /* can be used to debugging */
+
+var fullName = "Kristel Talimaa";
+
+console.log(fullName.length);   /* gives length of the string. reset trend is not to used the var, but use let! stands for variable also */
+console.log(fullName);
+
+let locationCity = "Tallinn";
+
+const ZIPCODE = 80041;      /* using const for constant and always use CAPS letters */
+
+// locationCity = "Tartu"; /* can change location value, but not zipcode value */
+
+let isActive = true;    /* can act as boolean */
+let age = 32;
+let salary = 4500.00;
+
+// Comparison
+let a = '5';    /* char */
+let b = 5;  /* int */
+
+
+if (a == b) {
+    console.log("A is same value as B.");       // value comparison
+} else if (a === b) {
+    console.log("A is same value and type as B.");      // value and type comparison
+}
+
+
+
+
+
