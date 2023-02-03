6756178b5ca38123cd640b556c9df58f35cfcc4f
ktalimaa <kristeltalimaa@gmail.com>
2023-02-03 14:39:53 +0200

    Fibonacci's homework added

--- assets/js/fibonacci-exercise.js
+++ assets/js/fibonacci-exercise.js
@@ -1,6 +1,19 @@
-let fibonacci = [0, 1];
+function fibonacci(x) {
+    let fibonacciNumber = [0, 1];
+    for (let i = 2; i <= x; i++) {
+        fibonacciNumber[i] = fibonacciNumber[i - 1] + fibonacciNumber[i - 2];
+    }
+    return fibonacciNumber[x];
+}
+
+console.log(fibonacci(9));
+
+
+// solution II
+let fibonacciSolution = [0, 1];
 
 for (let i = 2; i <= 10; i++) {
-    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    fibonacciSolution[i] = fibonacciSolution[i - 1] + fibonacciSolution[i - 2];
 }
-console.log(fibonacci[9]);
+console.log(fibonacciSolution[9]);
+
